abacc46cf01dcf7586eec03bba20015f45948c23
Christian Tschenett <cht@zuehlke.com>
2019-06-19 08:28:03 +0200

    switched to dockerized dependency

--- cloudwatch-watch-logs.sh
+++ cloudwatch-watch-logs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#pre-cond: pip install awslogs
 
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
+
+#pre-cond: pip install awslogs
+alias awslogs='docker run -v ~/.aws:/root/.aws --rm -i chtzdc/awslogs:latest'
 
 awslogs get /ecs/$STACK_PREFIX-ecs --watch 
