1aa46fbbfac02965ba109b3c7405e2402c3ace1a
Christian Tschenett <cht@zuehlke.com>
2019-06-19 16:04:05 +0200

    bugfix

--- cloudwatch-watch-logs.sh
+++ cloudwatch-watch-logs.sh
@@ -3,7 +3,4 @@
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
 
-#pre-cond: pip install awslogs
-alias awslogs='docker run -v ~/.aws:/root/.aws --rm -i chtzdc/awslogs:latest'
-
-awslogs get /ecs/$STACK_PREFIX-ecs --watch 
+docker run -v ~/.aws:/root/.aws --rm -i chtzdc/awslogs:latest get /ecs/$STACK_PREFIX-ecs --watch 
