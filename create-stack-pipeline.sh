e00522a0b9766e491818d8bb5fd874f2ef0b5c8d
Christian Tschenett <cht@zuehlke.com>
2019-07-11 12:29:42 +0200

    deploy pipeline (work in progress)

--- create-stack-pipeline.sh
+++ create-stack-pipeline.sh
@@ -9,8 +9,16 @@ REPO_URL=$(./get-stack-output.sh $STACK_PREFIX-ecr DockerRepoUrl)
 CODECOMMIT_ARN=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryARN)
 CODECOMMIT_NAME=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryName)
 
+CONTAINER_NAME=$STACK_PREFIX-ecs
+
+FARGATE_CLUSTER=$(./get-stack-output.sh $STACK_PREFIX-ecs FargateCluster)
+FARGATE_SERVICE=$(./get-stack-output.sh $STACK_PREFIX-ecs FargateService)
+
 aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,ParameterValue=$CODECOMMIT_ARN \
 	ParameterKey=CodeCommitRepositoryName,ParameterValue=$CODECOMMIT_NAME \
 	ParameterKey=DockerLocalTag,ParameterValue=$LOCAL_TAG \
-	ParameterKey=RepoUrl,ParameterValue=$REPO_URL
+	ParameterKey=RepoUrl,ParameterValue=$REPO_URL \
+	ParameterKey=ContainerName,ParameterValue=$CONTAINER_NAME \
+	ParameterKey=FargateCluster,ParameterValue=$FARGATE_CLUSTER \
+	ParameterKey=FargateService,ParameterValue=$FARGATE_SERVICE
