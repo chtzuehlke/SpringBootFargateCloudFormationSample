ce628f8978bb56925516945abb031450ce5baed3
Christian Tschenett <cht@zuehlke.com>
2019-07-11 13:11:31 +0200

    script cleanup

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -3,17 +3,6 @@
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
 
-LOCAL_TAG=chtzuehlke/sample-web-workload:latest
-REPO_URL=$(./get-stack-output.sh $STACK_PREFIX-ecr DockerRepoUrl)
-
-CODECOMMIT_ARN=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryARN)
-CODECOMMIT_NAME=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryName)
-
-CONTAINER_NAME=$STACK_PREFIX-ecs
-
-FARGATE_CLUSTER=$(./get-stack-output.sh $STACK_PREFIX-ecs FargateCluster)
-FARGATE_SERVICE=$(./get-stack-output.sh $STACK_PREFIX-ecs FargateService)
-
 aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,UsePreviousValue=true \
 	ParameterKey=CodeCommitRepositoryName,UsePreviousValue=true \
