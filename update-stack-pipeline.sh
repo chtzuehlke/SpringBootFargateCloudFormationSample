29ebee5edd6f6512fb1e9fca15bc052f9afec52e
Christian Tschenett <cht@zuehlke.com>
2019-07-11 14:45:45 +0200

    switched to cloudformatio deployment to avoid stack drifts

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -3,6 +3,8 @@
 PREFIX=${1:-default}
 STACK_PREFIX="helloworld-$PREFIX"
 
+DB_PASSWORD_PARAM_NAME=$STACK_PREFIX-db-pwd
+
 aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,UsePreviousValue=true \
 	ParameterKey=CodeCommitRepositoryName,UsePreviousValue=true \
@@ -11,4 +13,8 @@ aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STAC
 	ParameterKey=ContainerName,UsePreviousValue=true \
 	ParameterKey=FargateCluster,UsePreviousValue=true \
 	ParameterKey=FargateService,UsePreviousValue=true \
-	ParameterKey=FargateStackName,ParameterValue=$STACK_PREFIX-ecs
+	ParameterKey=FargateStackName,ParameterValue=$STACK_PREFIX-ecs \
+	ParameterKey=NetworkStack,ParameterValue=$STACK_PREFIX-sg \
+	ParameterKey=LoadBalancerStack,ParameterValue=$STACK_PREFIX-alb \
+	ParameterKey=DatabaseStack,ParameterValue=$STACK_PREFIX-rds \
+	ParameterKey=DBPassSSMName,ParameterValue=$DB_PASSWORD_PARAM_NAME
