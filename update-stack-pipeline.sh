fcc67b6fab26309b8eb239173824eea6ede0ce89
Christian Tschenett <cht@zuehlke.com>
2019-07-11 11:22:34 +0200

    pipeline (work in progress)

--- update-stack-pipeline.sh
+++ update-stack-pipeline.sh
@@ -9,7 +9,7 @@ REPO_URL=$(./get-stack-output.sh $STACK_PREFIX-ecr DockerRepoUrl)
 CODECOMMIT_ARN=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryARN)
 CODECOMMIT_NAME=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryName)
 
-aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
+aws cloudformation update-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,ParameterValue=$CODECOMMIT_ARN \
 	ParameterKey=CodeCommitRepositoryName,ParameterValue=$CODECOMMIT_NAME \
 	ParameterKey=DockerLocalTag,ParameterValue=$LOCAL_TAG \
