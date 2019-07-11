d7c7aa468663f77c4f8df69b784ae43c35d11278
Christian Tschenett <cht@zuehlke.com>
2019-07-11 11:12:11 +0200

    pipeline (work in progress)

--- create-stack-pipeline.sh
+++ create-stack-pipeline.sh
@@ -9,7 +9,7 @@ REMOTE_TAG=$(./get-stack-output.sh $STACK_PREFIX-ecr DockerRepoUrl):test
 CODECOMMIT_ARN=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryARN)
 CODECOMMIT_NAME=$(./get-stack-output.sh $STACK_PREFIX-git CodeCommitRepositoryName)
 
-aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-ecs --template-body file://cloudformation/pipeline.yaml --parameters \
+aws cloudformation create-stack --capabilities CAPABILITY_IAM --stack-name $STACK_PREFIX-pipe --template-body file://cloudformation/pipeline.yaml --parameters \
 	ParameterKey=CodeCommitRepositoryARN,ParameterValue=$CODECOMMIT_ARN \
 	ParameterKey=CodeCommitRepositoryName,ParameterValue=$CODECOMMIT_NAME \
 	ParameterKey=DockerLocalTag,ParameterValue=$LOCAL_TAG \
